7f32b90f45a37383a3998d95e67a0f2111324f8c
刘俊雄 <15858209984@163.com>
2018-08-21 11:12:51 +0800

    增加typescript 函数式组件

---
+++ template.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './{{template}}.styl';
+
+interface Props {
+
+}
+
+
+export default {{ template }} = (props:Props) => {
+  return <div className="{{template}}">
+        {{template}}
+  </div>
+}
