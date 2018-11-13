308e0e1932faa00c7b592e23fe12a4888d4003d5
liujunxiong <heicao.ljx@raycloud.com>
2018-11-13 20:17:00 +0800

    useState

--- template.js
+++ template.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import { useState ,useEffect} from 'react';
 import PropTypes from 'prop-types';
 import './{{template}}.styl';
 
 const {{ template }} = props => {
+  //  参数   方法
+  const [count, setCount] = useState(0);   
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`;
+  });
   return <div className="{{template}}">
-        {{template}}
+    {{ template }}
+    <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
   </div>
 }
 
