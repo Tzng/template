64680da5bc03b7e606304577c3594c715d2e81a2
Allahbin <16008@theling.cn>
2019-04-18 19:21:43 +0800

    格式化代码

--- Index.js
+++ Index.js
@@ -5,67 +5,65 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'dva';
+import {connect} from 'dva';
 import './{{template}}.less';
 
 /**
  * @descriptions: {{descriptions}}
  */
-@connect(({ loading, {{namespace}} }) => ({
-	{{namespace}},
-	loading: loading.models.{{namespace}},
+@connect(({loading, {{namespace}}}) =>(
+    {{{namespace}},
+    loading: loading.models.{{namespace}},
 }))
+
 class Index extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+    constructor(props) {
+        super(props);
+    }
 
-	state = {
+    state = {};
 
-	};
+    componentDidMount() {
+    }
 
-	// 接收到新的props的检测
-  	static getDerivedStateFromProps(nextProps, prevState){
-		return null;
-	}
+    // 接收到新的props的检测
+    static getDerivedStateFromProps(nextProps, prevState) {
+        return null;
+    }
 
-	componentDidMount() { }
+    shouldComponentUpdate(nextProps, nextState) {
+        return true;
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+    // 在render之前调用，state已更新
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        // 如果 `props.list` 增加，将原来的 scrollHeight 存入 listRef
+        if (prevProps.list.length < this.props.list.length) {
+            return this.listRef.scrollHeight
+        }
+        return null
+    }
 
 
-	
-	shouldComponentUpdate(nextProps, nextState) {
-		return true;
-	}
-
-	// 在render之前调用，state已更新
-	getSnapshotBeforeUpdate(prevProps, prevState) {
-		// 如果 `props.list` 增加，将原来的 scrollHeight 存入 listRef
-		if (prevProps.list.length < this.props.list.length) {
-			return this.listRef.scrollHeight
-		}
-		return null
-	}
-
-	componentDidUpdate(prevProps, prevState, snapshot) { }
-
-	componentWillUnmount() { }
-
-
-	render() {
-		return (
-			<div className="{{template}}">
-				我是{{ template }}页面
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div className="{{template}}">
+                我是{{template}}页面
+            </div>
+        );
+    }
 }
 
-Index.propTypes = {
+Index.propTypes = {};
 
-};
-
-Index.defaultProps = {
-
-};
+Index.defaultProps = {};
 
 export default Index
