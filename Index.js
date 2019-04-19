1592d048f2c657cd135205d7a8c73079313c2af4
Allahbin <16008@theling.cn>
2019-04-19 16:55:09 +0800

    删除了多余的生命周期

--- Index.js
+++ Index.js
@@ -11,9 +11,8 @@ import './{{template}}.less';
 /**
  * @descriptions: {{descriptions}}
  */
-@connect(({loading, {{namespace}}}) =>(
+@connect(({ {{namespace}} }) =>(
     {{{namespace}},
-    loading: loading.models.{{namespace}},
 }))
 class Index extends React.Component {
     constructor(props) {
@@ -23,34 +22,11 @@ class Index extends React.Component {
     state = {};
 
     componentDidMount() {
-    }
-
-    // 接收到新的props的检测
-    static getDerivedStateFromProps(nextProps, prevState) {
-        return null;
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        return true;
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
 
     }
-
     componentWillUnmount() {
 
     }
-
-    // 在render之前调用，state已更新
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-        // 如果 `props.list` 增加，将原来的 scrollHeight 存入 listRef
-        if (prevProps.list.length < this.props.list.length) {
-            return this.listRef.scrollHeight
-        }
-        return null
-    }
-
 
     render() {
         return (
